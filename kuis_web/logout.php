012c487ab1e0cb331e213d723b6d50d0fea23644
zakytaufiq <151290835+kytaufiq@users.noreply.github.com>
2025-05-22 19:56:46 +0700

    update

--- kuis_web/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();        // Mulai session
-session_destroy();      // Hapus semua data session
-header("Location: login.php"); // Kembali ke halaman login
-exit;
-?>
--- kuis_web/users/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../koneksi/db.php";
-
-
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM users WHERE id=$id");
-header("Location: list.php");
-?>
